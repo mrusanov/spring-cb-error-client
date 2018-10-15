d1517586764aefc8a6be8a0d85b6ff413849341b
mrusanov <mihail.rusanov93@gmail.com>
2018-10-15 18:51:44 +0300

    Added config server.

--- src/main/java/com/mrusanov/spring/cb/error/client/SpringCbErrorClientApplication.java
+++ src/main/java/com/mrusanov/spring/cb/error/client/SpringCbErrorClientApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@RefreshScope
 public class SpringCbErrorClientApplication {
 
     @Value("${client.server.readTimeoutInMilliseconds}")
